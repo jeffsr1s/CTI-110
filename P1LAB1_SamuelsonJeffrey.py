43bb9e8e25eb2690879c4bdda3759f6444750902
jeffsr1s <samuelsj8158@student.faytechcc.edu>
2025-04-05 21:08:43 -0400

    Add files via upload
    First upload for P1LAB2

---
+++ P1LAB1_SamuelsonJeffrey.py
@@ -0,0 +1,11 @@
+#Jeffrey Samuelson
+#4/5/2025
+#P1LAB1
+#Printing user input
+
+#Get the user input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+#Print the user input with statement
+print(f"Hello, {first_name} {last_name}! Welcome to CTI-110!")
